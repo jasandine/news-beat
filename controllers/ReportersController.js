8d5be52eecbbd6d869ea6c47614b49e68122c52c
Joey Sandine <student@epicodus.com>
2015-02-27 13:50:56 -0800

    Add age and experience input options for a reporter

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,7 +1,10 @@
 newsBeat.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId)
+
   $scope.addReporter = function() {
-    $scope.beat.reporters.push({ name: $scope.reporterName });
+    $scope.beat.reporters.push({ name: $scope.reporterName,
+                                 age: $scope.reporterAge,
+                                 experience: $scope.reporterExperience });
     $scope.reporterName = null;
   }
 });
