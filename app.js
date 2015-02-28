6d72fe7208f43c487ab25807659f6d98f4168944
Joey Sandine <student@epicodus.com>
2015-02-27 16:32:29 -0800

    Add a feature to click on a reporter to see their profile

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var newsBeat = angular.module('newsBeat', ['ui.router']);
 
 newsBeat.config(function($stateProvider) {
+
   $stateProvider.state('home', {
     url: "",
     templateUrl: "partials/home.html"
--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,12 +1,14 @@
 newsBeat.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId)
+  $scope.beats = BeatsFactory.beats;
 
   $scope.addReporter = function() {
     $scope.beat.reporters.push({ name: $scope.reporterName,
                                  birthday: $scope.reporterBirthday,
-                                 experience: $scope.reporterExperience });
+                                 experience: $scope.reporterExperience,
+                                 profile: [] });
     $scope.reporterName = null;
     $scope.reporterBirthday = null;
     $scope.reporterExperience = null;
-  }
+  };
 });
